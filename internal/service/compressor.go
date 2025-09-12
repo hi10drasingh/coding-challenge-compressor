b577797fc26217c28feb544b9f4ac860a2f319a6
hitendrarao <20615412+hitendrarao@users.noreply.github.com>
2025-09-12 16:25:19 +0530

    Added Tests for Count Freq Functionm

--- internal/service/compressor.go
+++ internal/service/compressor.go
@@ -9,3 +9,13 @@ func Decompress(data []byte) []byte {
 	// Dummy implementation of decompression
 	return data
 }
+
+func CharFrequency(s string) map[rune]int {
+	freq := make(map[rune]int)
+
+	for _, ch := range s {
+		freq[ch]++
+	}
+
+	return freq
+}
--- internal/service/compressor_test.go
+++ internal/service/compressor_test.go
@@ -1,6 +1,9 @@
 package service
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCompressor(t *testing.T) {
 	// Placeholder test
@@ -8,4 +11,58 @@ func TestCompressor(t *testing.T) {
 
 func TestDecompressor(t *testing.T) {
 	// Placeholder test
+}
+
+func TestCharFrequency(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected map[rune]int
+	}{
+		{
+			name:  "basic hello",
+			input: "Hello Gophers!",
+			expected: map[rune]int{
+				'H': 1,
+				'e': 2,
+				'l': 2,
+				'o': 2,
+				' ': 1,
+				'G': 1,
+				'p': 1,
+				'h': 1,
+				'r': 1,
+				's': 1,
+				'!': 1,
+			},
+		},
+		{
+			name:     "empty string",
+			input:    "",
+			expected: map[rune]int{},
+		},
+		{
+			name:  "repeated runes",
+			input: "aaa",
+			expected: map[rune]int{
+				'a': 3,
+			},
+		},
+		{
+			name:  "unicode runes",
+			input: "世世",
+			expected: map[rune]int{
+				'世': 2,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := CharFrequency(tt.input)
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("\ninput: %q\nexpected: %#v\ngot:      %#v", tt.input, tt.expected, result)
+			}
+		})
+	}
 }
